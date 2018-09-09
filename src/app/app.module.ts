b470374c3b8e7d79ec35689f18b1a7ad8b088f20
ankur garha <speedyankur@gmail.com>
2018-09-09 15:50:09 +0530

    Added Course List and Course Detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    CourseDetailComponent
   ],
   imports: [
     BrowserModule,
