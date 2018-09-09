744bc64e71275dc3064f00c7e68191e3c81c430c
ankur garha <speedyankur@gmail.com>
2018-09-09 16:36:01 +0530

    integarted course list component to course service and showed list of available courses

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseService } from '../service/course.service';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-course-list',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseListComponent implements OnInit {
 
-  constructor() { }
+	private courses = [];
+	private errorMessage;
+  	constructor(private _courseService: CourseService) { }
 
-  ngOnInit() {
-  }
-
+  	ngOnInit() {    
+	    this._courseService.getCourses()
+	      	.subscribe(data => {
+		      	let coursesRawData = data;
+	    		let onlyCourseSlugs = Object.keys(coursesRawData.courses);
+	    		for (let prop of onlyCourseSlugs) { 
+	    			let course = data.courses[prop];
+	    			course.slug = prop;
+	    	    	this.courses.push(course);
+	    		}
+	      	},error => this.errorMessage = error); 	
+  	}
 }
