b19a7b1c209391b645bd25225348f77c432eaad6
ankur garha <speedyankur@gmail.com>
2018-09-09 16:03:54 +0530

    Updated App routing module for /courses /courses/{slug/id}
    any other link will re route to PAGE NOT FOUND

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CourseListComponent } from './course-list/course-list.component';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '', 
+		redirectTo:'/courses', 
+		pathMatch: 'full'
+	},{
+		path:'courses',
+		component: CourseListComponent
+	},{
+		path:'courses/:slug',
+		component: CourseDetailComponent
+	},
+	{
+		path:'**',
+		component: PageNotFoundComponent
+	}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const RoutingComponents = [CourseListComponent,CourseDetailComponent, PageNotFoundComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './course-list/course-list.component';
 import { CourseDetailComponent } from './course-detail/course-detail.component';
@@ -10,9 +10,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    CourseDetailComponent,
-    PageNotFoundComponent
+    RoutingComponents
   ],
   imports: [
     BrowserModule,
